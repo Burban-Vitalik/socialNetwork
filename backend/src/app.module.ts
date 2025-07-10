1df2498436e4c9a98dbadaaaeb26af405eed5d70
Vitalik <burbanvitalik2002@gmail.com>
2025-07-10 19:20:07 +0300

    solver problem with eslint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ChatModule } from './chat/chat.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, ChatModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
